bb46c04e7ecd1974a0c4d1d05a8ed79eef3af0da
chunnuthakur <ck.8107@gmail.com>
2026-05-11 12:46:06 +0530

    chore: remove documents and test files from branch history and update .gitignore

--- test_franchise_get.js
+++
@@ -1,9 +0,0 @@
-fetch("http://localhost:5000/api/v1/franchise/franchise-inquiry", {
-  method: "GET",
-  headers: {
-    "Content-Type": "application/json",
-  }
-})
-.then(res => res.text())
-.then(result => console.log("Response:", result.slice(0, 500)))
-.catch(err => console.error("Error:", err));
